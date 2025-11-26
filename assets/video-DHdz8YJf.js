eebf6708a819ccebf04d570c963f7c55ac53b3cc
Barth-cyber <devnull@idl.com>
2025-11-27 01:15:52 +0530

    fix(gh-pages): make font paths relative (remove leading /fonts/) so app works under subpath

--- assets/video-DHdz8YJf.js
+++ assets/video-DHdz8YJf.js
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8" />
@@ -15,11 +15,11 @@
 
     <link rel="manifest" href="/manifest.json" />
 
-    <link rel="preload" href="/fonts/lato-400.woff2" as="font" type="font/woff2" crossorigin>
-    <link rel="preload" href="/fonts/lato-700.woff2" as="font" type="font/woff2" crossorigin>
-    <link rel="preload" href="/fonts/lato-900.woff2" as="font" type="font/woff2" crossorigin>
-    <link rel="preload" href="/fonts/inter.woff2" as="font" type="font/woff2" crossorigin>
-    <link rel="preload" href="/fonts/inter-italic.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="fonts/lato-400.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="fonts/lato-700.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="fonts/lato-900.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="fonts/inter.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="fonts/inter-italic.woff2" as="font" type="font/woff2" crossorigin>
 
     <title>QR Code Generator - Create Free QR Codes</title>
 
@@ -55,7 +55,7 @@
         font-style: normal;
         font-weight: 300;
         font-display: swap;
-        src: url(/fonts/lato-300-ext.woff2) format('woff2');
+        src: url(fonts/lato-300-ext.woff2) format('woff2');
         unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
       }
       /* latin */
@@ -64,7 +64,7 @@
         font-style: normal;
         font-weight: 300;
         font-display: swap;
-        src: url(/fonts/lato-300.woff2) format('woff2');
+        src: url(fonts/lato-300.woff2) format('woff2');
         unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
       }
       /* latin-ext */
@@ -73,7 +73,7 @@
         font-style: normal;
         font-weight: 400;
         font-display: swap;
-        src: url(/fonts/lato-400-ext.woff2) format('woff2');
+        src: url(fonts/lato-400-ext.woff2) format('woff2');
         unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
       }
       /* latin */
@@ -82,7 +82,7 @@
         font-style: normal;
         font-weight: 400;
         font-display: swap;
-        src: url(/fonts/lato-400.woff2) format('woff2');
+        src: url(fonts/lato-400.woff2) format('woff2');
         unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
       }
       /* latin-ext */
@@ -91,7 +91,7 @@
         font-style: normal;
         font-weight: 700;
         font-display: swap;
-        src: url(/fonts/lato-700-ext.woff2) format('woff2');
+        src: url(fonts/lato-700-ext.woff2) format('woff2');
         unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
       }
       /* latin */
@@ -100,7 +100,7 @@
         font-style: normal;
         font-weight: 700;
         font-display: swap;
-        src: url(/fonts/lato-700.woff2) format('woff2');
+        src: url(fonts/lato-700.woff2) format('woff2');
         unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
       }
       /* latin-ext */
@@ -109,7 +109,7 @@
         font-style: normal;
         font-weight: 900;
         font-display: swap;
-        src: url(/fonts/lato-900-ext.woff2) format('woff2');
+        src: url(fonts/lato-900-ext.woff2) format('woff2');
         unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
       }
       /* latin */
@@ -118,7 +118,7 @@
         font-style: normal;
         font-weight: 900;
         font-display: swap;
-        src: url(/fonts/lato-900.woff2) format('woff2');
+        src: url(fonts/lato-900.woff2) format('woff2');
         unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
       }
 
@@ -130,7 +130,7 @@
         font-style: italic;
         font-weight: 100 900;
         font-display: swap;
-        src: url(/fonts/inter-italic-ext.woff2) format('woff2');
+        src: url(fonts/inter-italic-ext.woff2) format('woff2');
         unicode-range: U+0100-02AF, U+0304, U+0308, U+0329, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20C0, U+2113, U+2C60-2C7F, U+A720-A7FF;
       }
       /* latin */
@@ -139,7 +139,7 @@
         font-style: italic;
         font-weight: 100 900;
         font-display: swap;
-        src: url(/fonts/inter-italic.woff2) format('woff2');
+        src: url(fonts/inter-italic.woff2) format('woff2');
         unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
       }
       /* latin-ext */
@@ -148,7 +148,7 @@
         font-style: normal;
         font-weight: 100 900;
         font-display: swap;
-        src: url(/fonts/inter-ext.woff2) format('woff2');
+        src: url(fonts/inter-ext.woff2) format('woff2');
         unicode-range: U+0100-02AF, U+0304, U+0308, U+0329, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20C0, U+2113, U+2C60-2C7F, U+A720-A7FF;
       }
       /* latin */
@@ -157,7 +157,7 @@
         font-style: normal;
         font-weight: 100 900;
         font-display: swap;
-        src: url(/fonts/inter.woff2) format('woff2');
+        src: url(fonts/inter.woff2) format('woff2');
         unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
       }
     </style>
